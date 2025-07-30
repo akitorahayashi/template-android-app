04ae300dde3e124d098921d04d6217066f8dd21e
akitorahayashi <atora.has75@gmail.com>
2025-07-30 11:12:54 +0900

    ビルドを通した

--- app/src/main/java/com/akitorahayashi/templateandroidapp/MainActivity.kt
+++ app/src/main/java/com/akitorahayashi/templateandroidapp/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.akitorahayashi.templateandroidapp
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.akitorahayashi.templateandroidapp.ui.theme.TemplateAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TemplateAndroidAppTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding),
                     )
@@ -44,7 +43,7 @@ fun greeting(
 @Preview(showBackground = true)
 @Composable
 fun greetingPreview() {
-    TemplateAndroidAppTheme {
-        Greeting("Android")
+    MaterialTheme {
+        greeting("Android")
     }
 }
